3b7500df1eb76c22849dcb31dc2a89b8d94ce552
nomiking3254 <140294436+nomiking3254@users.noreply.github.com>
2024-02-15 19:32:55 +0500

    change

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let one = document.getElementById('one');
 let two = document.getElementById('two');
 let three = document.getElementById('three');
+let four = document.getElementById('four');
+let down = document.getElementById('down');
 let ones = document.getElementById('ones');
 let twos = document.getElementById('twos');
+let fours = document.getElementById('fours');
 let threeds = document.getElementById('threes');
+let downs = document.getElementById('downs');
 one.addEventListener('mousemove', function () {
-    ones.style.display = 'block';
+    ones.style.display = 'flex';
 })
 ones.addEventListener('mouseleave', function () {
     ones.style.display = 'none';
@@ -21,5 +25,27 @@ three.addEventListener('mouseover', function () {
     threes.style.display = 'flex';
 })
 threes.addEventListener('mouseout', function () {
-    threes.style.display = 'none';
+    fours.style.display = 'none';
+})
+four.addEventListener('mouseover', function () {
+    fours.style.display = 'flex';
+})
+fours.addEventListener('mouseover', function () {
+    fours.style.display = 'flex';
+})
+fours.addEventListener('mouseleave', function () {
+    fours.style.display = 'none';
+})
+
+
+
+
+down.addEventListener('mouseover', function () {
+    downs.style.display = 'block';
+})
+downs.addEventListener('mouseover', function () {
+    downs.style.display = 'block';
+})
+downs.addEventListener('mouseleave', function () {
+    downs.style.display = 'none';
 })
